961d1ef82e7bf8bc0e5b77af1e5298251490b1e1
maythammjd <104466328+maythammjd@users.noreply.github.com>
2023-07-20 14:52:54 +1200

    v2

--- guesswhere.py
+++ guesswhere.py
@@ -1,4 +1,6 @@
 import random
+import tkinter as tk
+from tkinter import messagebox
 
 # Define a list of countries and their capitals
 countries = {
@@ -25,51 +27,88 @@ countries = {
 }
 
 # Define a function to administer the quiz
-def country_quiz():
-    # Keep track of the score
-    score = 0
-    
+def guesswhere_quiz():
+    # Create a Tkinter window
+    window = tk.Tk()
+    window.title("Guesswhere")
+
+    # Create a score label
+    score_label = tk.Label(window, text="Score: 0")
+    score_label.pack()
+
     # Shuffle the list of countries
     country_list = list(countries.items())
     random.shuffle(country_list)
-    
-    # Iterate over the shuffled list of countries
-    for country, capital in country_list[:4]:
-        # Print the question
-        print("What is the capital of", country + "?")
-        
+
+    # Keep track of the score
+    score = 0
+
+    # Define a function to handle answer submission
+    def submit_answer(user_answer, capital):
+        nonlocal score
+
+        # Check if the answer is correct
+        if user_answer.lower() == capital.lower():
+            score += 1
+
+        # Update the score label
+        score_label.config(text="Score: {}".format(score))
+
+        # Clear the entry field
+        answer_entry.delete(0, tk.END)
+
+        # Move to the next question
+        ask_question()
+
+    # Define a function to ask a new question
+    def ask_question():
+        # Check if all questions have been asked
+        if len(country_list) == 0:
+            # Display the final score in a message box
+            messagebox.showinfo("Quiz Complete", "Quiz completed! Your final score is {} out of 4.".format(score))
+            # Close the window
+            window.destroy()
+            return
+
+        # Get the next country and capital
+        country, capital = country_list.pop()
+
+        # Clear the question frame
+        for widget in question_frame.winfo_children():
+            widget.destroy()
+
+        # Create the question label
+        question_label = tk.Label(question_frame, text="What is the capital of {}?".format(country))
+        question_label.pack()
+
         # Define a list of answer options
         answer_options = [capital]
-        
+
         # Add three random capitals to the answer options
         while len(answer_options) < 4:
             random_country, random_capital = random.choice(list(countries.items()))
             if random_capital not in answer_options:
                 answer_options.append(random_capital)
-        
+
         # Shuffle the answer options
         random.shuffle(answer_options)
-        
-        # Print the answer options
+
+        # Create the answer option buttons
+        option_buttons = []
         for i, option in enumerate(answer_options):
-            print(chr(i + 97) + ") " + option)
-        
-        # Get the user's answer
-        user_answer = input("Enter your answer (a, b, c, or d): ")
-        
-        # Check if the answer is correct
-        if user_answer.lower() == "a" and answer_options[0] == capital:
-            score += 1
-        elif user_answer.lower() == "b" and answer_options[1] == capital:
-            score += 1
-        elif user_answer.lower() == "c" and answer_options[2] == capital:
-            score += 1
-        elif user_answer.lower() == "d" and answer_options[3] == capital:
-            score += 1
-    
-    # Print the final score
-    print("Your score is:", score, "out of 4")
-    
+            button = tk.Button(question_frame, text=option, command=lambda option=option: submit_answer(option, capital))
+            button.pack()
+            option_buttons.append(button)
+
+    # Create the question frame
+    question_frame = tk.Frame(window)
+    question_frame.pack(pady=20)
+
+    # Start the quiz
+    ask_question()
+
+    # Run the Tkinter event loop
+    window.mainloop()
+
 # Call the quiz function
-country_quiz()
-In this version of the quiz, the questions are randomly selected from a list of countries and their capitals, and the answer options are also randomly generated. The user is presented with four answer options (labeled a, b, c, and d), and their score is based on how many questions they answer correctly out of the four presented.
+guesswhere_quiz()
